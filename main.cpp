fae79be534134c8fa7dd20498739afef969047c9
Tastypotato245 <rbt2775@cau.ac.kr>
2024-10-12 20:29:10 +0900

    add: main

---
+++ main.cpp
@@ -0,0 +1,364 @@
+//
+// Display a color cube
+//
+// Colors are assigned to each vertex and then the rasterizer interpolates
+//   those colors across the triangles.  We us an orthographic projection
+//   as the default projetion.
+
+// 20204946 Kyusung Lee
+
+#include "cube.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/transform.hpp"
+#include <iostream>
+
+glm::mat4 projectMat;
+glm::mat4 viewMat;
+
+GLuint pvmMatrixID;
+
+float rotAngle = 0.0f;
+int isStop = false;
+int viewMode = 1;
+
+typedef glm::vec4  color4;
+typedef glm::vec4  point4;
+
+const int NumVertices = 36; //(6 faces)(2 triangles/face)(3 vertices/triangle)
+
+point4 points[NumVertices];
+color4 colors[NumVertices];
+
+// Vertices of a unit cube centered at origin, sides aligned with axes
+point4 vertices[8] = {
+	point4(-0.5, -0.5, 0.5, 1.0),
+	point4(-0.5, 0.5, 0.5, 1.0),
+	point4(0.5, 0.5, 0.5, 1.0),
+	point4(0.5, -0.5, 0.5, 1.0),
+	point4(-0.5, -0.5, -0.5, 1.0),
+	point4(-0.5, 0.5, -0.5, 1.0),
+	point4(0.5, 0.5, -0.5, 1.0),
+	point4(0.5, -0.5, -0.5, 1.0)
+};
+
+// RGBA colors
+color4 vertex_colors[8] = {
+	color4(0.0, 0.0, 0.0, 1.0),  // black
+	color4(0.0, 1.0, 1.0, 1.0),   // cyan
+	color4(1.0, 0.0, 1.0, 1.0),  // magenta
+	color4(1.0, 1.0, 0.0, 1.0),  // yellow
+	color4(1.0, 0.0, 0.0, 1.0),  // red
+	color4(0.0, 1.0, 0.0, 1.0),  // green
+	color4(0.0, 0.0, 1.0, 1.0),  // blue
+	color4(1.0, 1.0, 1.0, 1.0)  // white
+};
+
+//----------------------------------------------------------------------------
+
+// quad generates two triangles for each face and assigns colors
+//    to the vertices
+int Index = 0;
+void
+quad(int a, int b, int c, int d)
+{
+	colors[Index] = vertex_colors[a]; points[Index] = vertices[a];  Index++;
+	colors[Index] = vertex_colors[b]; points[Index] = vertices[b];  Index++;
+	colors[Index] = vertex_colors[c]; points[Index] = vertices[c];  Index++;
+	colors[Index] = vertex_colors[a]; points[Index] = vertices[a];  Index++;
+	colors[Index] = vertex_colors[c]; points[Index] = vertices[c];  Index++;
+	colors[Index] = vertex_colors[d]; points[Index] = vertices[d];  Index++;
+}
+
+//----------------------------------------------------------------------------
+
+// generate 12 triangles: 36 vertices and 36 colors
+void
+colorcube()
+{
+	quad(1, 0, 3, 2);
+	quad(2, 3, 7, 6);
+	quad(3, 0, 4, 7);
+	quad(6, 5, 1, 2);
+	quad(4, 5, 6, 7);
+	quad(5, 4, 0, 1);
+}
+
+//----------------------------------------------------------------------------
+
+// OpenGL initialization
+void
+init()
+{
+	colorcube();
+
+	// Create a vertex array object
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// Create and initialize a buffer object
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points) + sizeof(colors),
+		NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(points), points);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors);
+
+	// Load shaders and use the resulting shader program
+	GLuint program = InitShader("src/vshader.glsl", "src/fshader.glsl");
+	glUseProgram(program);
+
+	// set up vertex arrays
+	GLuint vPosition = glGetAttribLocation(program, "vPosition");
+	glEnableVertexAttribArray(vPosition);
+	glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0,
+		BUFFER_OFFSET(0));
+
+	GLuint vColor = glGetAttribLocation(program, "vColor");
+	glEnableVertexAttribArray(vColor);
+	glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0,
+		BUFFER_OFFSET(sizeof(points)));
+
+	pvmMatrixID = glGetUniformLocation(program, "mPVM");
+
+	projectMat = glm::perspective(glm::radians(65.0f), 1.0f, 0.1f, 100.0f);
+	viewMat = glm::lookAt(glm::vec3(0, 0, 2), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+}
+
+//----------------------------------------------------------------------------
+
+void drawMan(glm::mat4 manMat)
+{
+	glm::mat4 modelMat, pvmMat;
+
+	glm::mat4 legModelMat[2];
+	glm::mat4 armModelMat[2];
+	glm::mat4 setBodyMat;
+
+	glm::vec3 headPos;
+	glm::vec3 headScale;
+	glm::vec3 bodyPos;
+	glm::vec3 bodyScale;
+	glm::vec3 upperlegPos[2];
+	glm::vec3 lowerlegPos[2];
+	glm::vec3 armPos[2];
+	glm::vec3 forearmPos[2];
+	glm::vec3 partsScale;
+
+	bodyPos = glm::vec3(0.0, 0.0, 0.0); // body
+	bodyScale = glm::vec3(0.48, 0.24, 0.6); // body
+
+	headScale = glm::vec3(0.3, 0.3, 0.3); // head
+	headPos = glm::vec3(0.0, 0.0, bodyScale.z / 2 + headScale.z / 2); // head
+
+	partsScale = glm::vec3(0.24, 0.24, 0.3); // left upper leg
+
+	upperlegPos[0] = glm::vec3((partsScale.x / 2), 0.0, -(bodyScale.z / 2) + -(partsScale.x / 2)); // left upper leg
+	upperlegPos[1] = glm::vec3(-(partsScale.x / 2), 0.0, -(bodyScale.z / 2) + -(partsScale.x / 2)); // right upper leg
+	lowerlegPos[0] = glm::vec3(0.0, 0.0, -partsScale.z); // left lower leg
+	lowerlegPos[1] = glm::vec3(0.0 , 0.0, -partsScale.z); // right lower leg
+
+	armPos[0] = glm::vec3(-(bodyScale.x / 2 + partsScale.x / 2), 0.0, partsScale.z / 2 - 0.01); // left arm
+	armPos[1] = glm::vec3((bodyScale.x / 2 + partsScale.x / 2), 0.0, partsScale.z / 2 - 0.01); // right arm
+	forearmPos[0] = glm::vec3(0.0, 0.0, -partsScale.z); // left forearm
+	forearmPos[1] = glm::vec3(0.0, 0.0, -partsScale.z); // right forearm
+
+	// body
+	modelMat = glm::translate(manMat, bodyPos + glm::vec3(0.0, 0.0, 0.08f * cos(rotAngle * 2) + 0.08f));  //P*V*C*T*S*v
+	modelMat = glm::rotate(modelMat, 0.2f * cos(rotAngle), glm::vec3(0.0f, 0.0f, 1.0f));
+	setBodyMat = modelMat;
+	modelMat = glm::scale(modelMat, bodyScale);
+	pvmMat = projectMat * viewMat * modelMat;
+	glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+
+	// head
+	modelMat = glm::translate(setBodyMat, headPos);  //P*V*C*T*S*v
+	modelMat = glm::rotate(modelMat, -0.2f * cos(rotAngle), glm::vec3(0.0f, 0.0f, 1.0f));
+	modelMat = glm::scale(modelMat, headScale);
+	pvmMat = projectMat * viewMat * modelMat;
+	glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+
+	// upperleg
+	for (int i = 0; i < 2; ++i)
+	{
+		modelMat = glm::translate(setBodyMat, upperlegPos[i]);  //P*V*C*T*S*v
+
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, 0.1));
+		modelMat = glm::rotate(modelMat, (2 * i - 1) * 1.0f * cos(rotAngle), glm::vec3(1.0f, 0.0f, 0.0f));
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, -0.1));
+
+		legModelMat[i] = modelMat;
+		modelMat = glm::scale(modelMat, partsScale);
+		pvmMat = projectMat * viewMat * modelMat;
+		glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+	}
+	// lowerleg
+	for (int i = 0; i < 2; ++i)
+	{
+		modelMat = glm::translate(legModelMat[i], lowerlegPos[i]);  //P*V*C*T*S*v
+
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, 0.1));
+		modelMat = glm::rotate(modelMat, (2 * i - 1) * 0.4f * cos(rotAngle) + 0.4f, glm::vec3(1.0f, 0.0f, 0.0f));
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, -0.1));
+
+		modelMat = glm::scale(modelMat, partsScale);
+		pvmMat = projectMat * viewMat * modelMat;
+		glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+	}
+
+	// arm
+	for (int i = 0; i < 2; ++i)
+	{
+		modelMat = glm::translate(setBodyMat, armPos[i]);  //P*V*C*T*S*v
+
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, 0.1));
+		modelMat = glm::rotate(modelMat, (2 * i - 1) * 0.7f * cos(rotAngle), glm::vec3(1.0f, 0.0f, 0.0f));
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, -0.1));
+
+		armModelMat[i] = modelMat;
+		modelMat = glm::scale(modelMat, partsScale);
+		pvmMat = projectMat * viewMat * modelMat;
+		glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+	}
+	// forearm
+	for (int i = 0; i < 2; ++i)
+	{
+		modelMat = glm::translate(armModelMat[i], forearmPos[i]);  //P*V*C*T*S*v
+
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, 0.1));  //P*V*C*T*S*v
+		modelMat = glm::rotate(modelMat, (2 * i - 1) * 0.4f * cos(rotAngle) - 0.4f, glm::vec3(1.0f, 0.0f, 0.0f));
+		modelMat = glm::translate(modelMat, glm::vec3(0.0f, 0.0f, -0.1));  //P*V*C*T*S*v
+
+		modelMat = glm::scale(modelMat, partsScale);
+		pvmMat = projectMat * viewMat * modelMat;
+		glUniformMatrix4fv(pvmMatrixID, 1, GL_FALSE, &pvmMat[0][0]);
+		glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+	}
+}
+
+
+void display(void)
+{
+	glm::mat4 worldMat, pvmMat;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	if (viewMode == 1)			//side view
+	{
+		worldMat = glm::rotate(glm::mat4(1.0f), -1.57f, glm::vec3(0.0f, 0.0f, 1.0f));
+		worldMat = glm::rotate(worldMat, -1.57f, glm::vec3(0.0f, 1.0f, 0.0f));
+		worldMat = glm::rotate(worldMat, 0.1f, glm::vec3(1.0f, 0.0f, 0.0f));
+	}
+	else if (viewMode == 2)		// over the shoulder
+	{
+		worldMat = glm::rotate(glm::mat4(1.0f), -1.57f, glm::vec3(0.0f, 0.0f, 1.0f));
+		worldMat = glm::rotate(worldMat, -1.57f, glm::vec3(0.0f, 1.0f, 0.0f));
+		worldMat = glm::rotate(worldMat, -1.57f, glm::vec3(0.0f, 0.0f, 1.0f));
+		worldMat = glm::rotate(worldMat, -0.4f, glm::vec3(1.0f, 0.0f, 0.0f));
+		worldMat = glm::rotate(worldMat, 0.3f, glm::vec3(0.0f, 0.0f, 1.0f));
+	}
+	else if (viewMode == 3)		// front view
+	{
+		worldMat = glm::rotate(glm::mat4(1.0f), -1.2f, glm::vec3(1.0f, 0.0f, 0.0f));
+		worldMat = glm::rotate(worldMat, -0.3f, glm::vec3(0.0f, 0.0f, 1.0f));
+	}
+
+	drawMan(worldMat);
+	glutSwapBuffers();
+}
+
+//----------------------------------------------------------------------------
+
+void idle()
+{
+	static int prevTime = glutGet(GLUT_ELAPSED_TIME);
+	int currTime = glutGet(GLUT_ELAPSED_TIME);
+
+	if (abs(currTime - prevTime) >= 20)
+	{
+		if (!isStop)
+		{
+			float t = abs(currTime - prevTime);
+			rotAngle += glm::radians(t * 360.0f / 800.0f);
+			prevTime = currTime;
+			glutPostRedisplay();
+		}
+		else
+		{
+			prevTime = currTime;
+			glutPostRedisplay();
+		}
+	}
+}
+
+//----------------------------------------------------------------------------
+
+void
+keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 's': case 'S':
+		isStop = !isStop;
+		break;
+	case '1':
+		viewMode = 1;
+		break;
+	case '2':
+		viewMode = 2;
+		break;
+	case '3':
+		viewMode = 3;
+		break;
+	case 033:  // Escape key
+	case 'q': case 'Q':
+		exit(EXIT_SUCCESS);
+		break;
+	}
+}
+
+//----------------------------------------------------------------------------
+
+void resize(int w, int h)
+{
+	float ratio = (float)w / (float)h;
+	glViewport(0, 0, w, h);
+
+	projectMat = glm::perspective(glm::radians(65.0f), ratio, 0.1f, 100.0f);
+
+	glutPostRedisplay();
+}
+
+//----------------------------------------------------------------------------
+
+int
+main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(512, 512);
+	glutInitContextVersion(3, 2);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutCreateWindow("Cubeman Running");
+
+	glewInit();
+
+	init();
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutReshapeFunc(resize);
+	glutIdleFunc(idle);
+
+	glutMainLoop();
+	return 0;
+}
+
